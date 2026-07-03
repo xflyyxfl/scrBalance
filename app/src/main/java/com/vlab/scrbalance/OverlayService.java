b3aa5a04c5e83c5a6c885575239be188f9e4c1f3
xflyyxfl <xflyyxfl@qq.com>
2026-07-03 12:59:30 +0800

    fix:编译需要import报错问题

--- app/src/main/java/com/vlab/scrbalance/OverlayService.java
+++ app/src/main/java/com/vlab/scrbalance/OverlayService.java
@@ -4,6 +4,7 @@ import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
 import android.database.ContentObserver;
 import android.graphics.PixelFormat;
